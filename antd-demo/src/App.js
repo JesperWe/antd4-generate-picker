b8e2a352449f5d02ae2902d6df558a6378b0dcbc
Jesper Weissglas <jesper@journeyman.se>
2020-11-18 12:50:28 +0100

    Add explicit dayjs call.

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { ConfigProvider } from 'antd'
 import svSE from 'antd/lib/locale/sv_SE'
-
+import dayjs from 'dayjs'
+import 'dayjs/locale/sv'
 import { DatePicker, Calendar } from './components'
 import './App.css'
 
 const { MonthPicker, RangePicker, WeekPicker } = DatePicker
+
+dayjs.locale( 'sv' )
 
 function App() {
 	function onChange( date, dateString ) {
